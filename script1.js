213d32bc919f66e076fcc617a9178e716249e9ca
Luna <erahinpachecoluna@gmail.com>
2026-08-31 15:55:08 -0600

    probando agentes de codigo

--- script1.js
+++ script1.js
@@ -6,3 +6,14 @@ suma = x + y;
 console.log(`La suma es: ${suma}`);
 
 let arrayPets = ["michi", "puppy", "camaleon"];
+
+for (let index in arrayPets) {
+    console.log(arrayPets[index]);
+}
+arrayPets.forEach((pet) => {
+    console.log(pet);
+});
+
+
+
+
